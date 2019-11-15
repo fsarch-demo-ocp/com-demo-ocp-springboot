45c820d0351127536e664fa38b67cc450ebb11f9
Jermaine C. Edwards <jedward2@us.ibm.com>
2019-11-15 13:35:03 -0600

    Initial version

---
+++ src/main/java/com/demo/ocp/ComDemoOcpSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComDemoOcpSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComDemoOcpSpringbootApplication.class, args);
+	}
+
+}
